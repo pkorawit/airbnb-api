31825db496a590aa6f3a4dc844d2fe5cdfdc0864
Korawit <korawit.p@gmail.com>
2020-03-13 16:24:54 +0700

    Set server port

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+// set server port
 const port = process.env.PORT || 3000;
 const mongoose = require('mongoose');
 const Place = require('./models/place');
